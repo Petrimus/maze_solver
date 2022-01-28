65c0a24b51d96f5452a27a86435c5805594529b8
Petri Palmu <petrimus76@gmail.com>
2022-01-28 19:16:40 +0200

    first maze drawn. no creation, just 2-dim array

---
+++ app/src/main/java/mazesolver/Maze.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mazesolver;
+
+/**
+ *
+ * @author popalmu
+ */
+public class Maze {
+
+    int[][] maze
+            = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1},
+            {1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1},
+            {1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1},
+            {1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+
+            };
+
+    public void drawMaze() {
+        for (int[] maze1 : maze) {
+            System.out.println("");
+            for (int j = 0; j < maze1.length; j++) {
+                if (maze1[j] == 1) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/mazesolver/UserInterface.java
+++ app/src/main/java/mazesolver/UserInterface.java
@@ -19,23 +19,30 @@ public class UserInterface {
     }
 
     public void start() {
+        Maze maze = new Maze();
+
         System.out.print("Anna nimesi: ");
         String name = reader.nextLine();
         System.out.println("");
-        
+
         OUTER:
         while (true) {
             System.out.println("Anna komento: ");
             System.out.println("1 - tulosta nimi");
+            System.out.println("3 - piirrä labyrintti");
             System.out.println("2 - lopeta");
             int command = Integer.parseInt(reader.nextLine());
-            
+
             switch (command) {
                 case 1:
                     System.out.println(name);
                     break;
                 case 2:
                     break OUTER;
+                case 3:                        
+                    maze.drawMaze();
+                    System.out.println("");
+                    break;                            
                 default:
                     System.out.println("Syötä oikea komento");
                     break;
